4f96083ac265a52740e96ffab29fb6757f4a3d3f
Kauri Kont-Kontson <kaurikk@gmail.com>
2015-04-07 17:25:34 +0300

    Initial implementation

---
+++ src/FormFactory/SymfonyFormFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Managlea\Component\FormFactory;
+
+
+use Managlea\Component\FormFactoryInterface;
+use Symfony\Component\Form\FormFactory;
+
+class SymfonyFormFactory extends FormFactory implements FormFactoryInterface;
+{
+
+}
---
+++ src/FormFactoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Managlea\Component;
+
+
+interface FormFactoryInterface
+{
+    //
+}
