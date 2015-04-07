656d47c4274b5f3e962552a28ac7402df28e08f6
Kauri Kont-Kontson <kaurikk@gmail.com>
2015-04-07 17:36:32 +0300

    Resolved scrutinizer-ci issue

--- src/FormFactory/SymfonyFormFactory.php
+++ src/FormFactory/SymfonyFormFactory.php
@@ -6,7 +6,7 @@ namespace Managlea\Component\FormFactory;
 use Managlea\Component\FormFactoryInterface;
 use Symfony\Component\Form\FormFactory;
 
-class SymfonyFormFactory extends FormFactory implements FormFactoryInterface;
+class SymfonyFormFactory extends FormFactory implements FormFactoryInterface
 {
 
 }
